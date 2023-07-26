fb327c2b30f2f82c0022082a4b4f7a95e5a94ff1
kai._.mjy <kaimjy@kaist.ac.kr>
2023-07-26 21:15:39 +0900

    2023-07-26 21:12 Jiyeon 간단한 오타 수정12344512345

--- src/main/kotlin/madcamp/four/meanwhile/serviceImp/BookmarkServiceImp.kt
+++ src/main/kotlin/madcamp/four/meanwhile/serviceImp/BookmarkServiceImp.kt
@@ -16,12 +16,12 @@ class BookmarkServiceImp: BookmarkService {
     //구현 기능
     //1. like button 눌렀을 때 -> userid 별로 추가
 
-    override fun addBookmark(userId: Long, refLink: String, refTitle:String) {
-        print(userId)
-        print(refTitle)
-        print(refLink)
+    override fun addBookmark(bookmark: Bookmark) {
+        print(bookmark.userId)
+        print(bookmark.refTitle)
+        print(bookmark.refLink)
 //        bookmarkMapper.addBookmark(userId, refLink, refTitle)
-        bookmarkMapper.addBookmark(1234, "gg", "HAAA")
+        bookmarkMapper.addBookmark(bookmark)
 
 
     }
